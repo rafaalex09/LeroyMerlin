98026ec75a47922d61d35b108eabc86e1c7d5d7c
Alexandru Rafa <Alexandru.Rafa@endava.com>
2021-03-19 00:38:27 +0200

    Second commit

--- src/test/java/ro/leroymerlin/AppTest.java
+++ src/test/java/ro/leroymerlin/AppTest.java
@@ -3,8 +3,12 @@ package ro.leroymerlin;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 
 public class AppTest 
@@ -16,8 +20,13 @@ public class AppTest
         driver = new ChromeDriver();
         driver.get("https://www.leroymerlin.ro/");
         driver.manage().window().maximize();
+        WebElement inspiratieLink = driver.findElement(By.linkText("INSPIRATIE"));
+        inspiratieLink.click();
+
+//        List<WebElement> links = driver.findElements(By.tagName("a"));
+//        System.out.println(links.size());
         System.out.println(driver.getTitle());
-        driver.quit();
+//        driver.quit();
     }
     public static void main(String args[])
     {
